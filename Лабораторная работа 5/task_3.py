6078b6b54c89265d58ce0b399a3164be78fe66a2
gubinaai <115162581+gubinaai@users.noreply.github.com>
2022-11-12 15:35:10 +0300

    Update task_3.py

--- Лабораторная работа 5/task_3.py
+++ Лабораторная работа 5/task_3.py
@@ -1,9 +1,12 @@
 import random
 
+list_length = 15
+from_range = -10
+to_range = 10
 def get_unique_list_numbers() -> list[int]:
     list_unique_numbers = []
-    while len(list_unique_numbers) != 15:
-        number = random.randrange(-10, 10)
+    while len(list_unique_numbers) != list_length:
+        number = random.randrange(from_range, to_range)
         if number not in list_unique_numbers:
             list_unique_numbers.append(number)
     return list_unique_numbers
